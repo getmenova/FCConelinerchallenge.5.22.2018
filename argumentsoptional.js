2092b0b1d24afcd5151d571430d1b765ed49d71d
Michael A Nowiszewski <micski@gmail.com>
2018-05-22 17:04:10 +0000

    corrected filename

--- argumentsoptional.js
+++
@@ -1,4 +0,0 @@
-function convertHTML(str) {
-	str = str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, "&apos;");
-	return str;
-} // redone to use replace method
---
+++ convertHTML.js
@@ -0,0 +1,4 @@
+function convertHTML(str) {
+	str = str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, "&apos;");
+	return str;
+} // redone to use replace method
